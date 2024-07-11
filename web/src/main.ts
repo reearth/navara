e9ce0041eb1e395e25899fc84142b0a7575dcc49
keiya01 <34934510+keiya01@users.noreply.github.com>
2024-07-11 23:41:29 +0900

    format

--- web/src/main.ts
+++ web/src/main.ts
@@ -26,15 +26,14 @@ const tileUrls = {
   openstreetmap: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
   gsiStd: "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png",
   gsiSeamlessphoto: "https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg",
-}
+};
 
 const terrainUrls = {
   gsi: "https://cyberjapandata.gsi.go.jp/xyz/dem_png/{z}/{x}/{y}.png",
   mapbox: `https://api.mapbox.com/v4/mapbox.terrain-rgb/{z}/{x}/{y}.pngraw?access_token=${
     import.meta.env.NAVARA_MAPBOX_ACCESS_TOKEN
-  }`
-}
-
+  }`,
+};
 
 // const chiyodaExtent = {
 //   west: 139.712,
