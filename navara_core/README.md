2fa0d8570f3a389a98eb7dead97064d2aec38952
keiya01 <34934510+keiya01@users.noreply.github.com>
2024-07-17 18:42:05 +0900

    doc: organize a basic structure

---
+++ navara_core/README.md
@@ -0,0 +1,3 @@
+# Navara Core
+
+This is an utility library for GIS specific math.
--- navara_quadtree/src/traits.rs
+++ navara_quadtree/src/traits.rs
@@ -25,8 +25,11 @@ where
     U: PrimInt + Default + Sync + Send + 'static,
     T: Sync + Send + 'static + Debug,
 {
+    /// Initialize a leaf with specified coordinates.
+    /// The value which is made by `init` is stored internally as a custom value.
     fn initialize_leaf(&mut self, coords: Coords<U>, init: &dyn Fn(Coords<U>) -> T);
 
+    /// Initialize a root leaf.
     fn initialize_zero(&mut self, init: &dyn Fn(Coords<U>) -> T) {
         let x = U::zero();
         let y = U::zero();
@@ -34,6 +37,7 @@ where
         self.initialize_leaf((x, y, z), init);
     }
 
+    /// Initialize children of specified coordinates.
     fn initialize_children(&mut self, (x, y, z): Coords<U>, init: &dyn Fn(Coords<U>) -> T) {
         for i in 0..4 {
             let i = to_int::<usize, U>(i);
@@ -44,6 +48,7 @@ where
         }
     }
 
+    /// Get children, or create children and return it if the number of children isn't four.
     fn get_or_create_children(
         &mut self,
         (x, y, z): Coords<U>,
@@ -69,16 +74,20 @@ where
         children
     }
 
+    /// Get a leaf by specified coordinates.
     fn leaf(&self, coords: Coords<U>) -> Option<Box<dyn GeoSpacialQuadLeaf<U>>>;
 
+    /// Get a root leaf.
     fn zero(&self) -> Option<Box<dyn GeoSpacialQuadLeaf<U>>> {
         self.leaf((U::zero(), U::zero(), U::zero()))
     }
 
+    /// Get a parent of specified coordinates.
     fn parent(&self, (x, y, z): Coords<U>) -> Option<Box<dyn GeoSpacialQuadLeaf<U>>> {
         self.leaf((x >> 1, y >> 1, z - U::one()))
     }
 
+    /// Get childre of specified coordinates.
     fn children(&self, (x, y, z): Coords<U>) -> Option<Vec<Box<dyn GeoSpacialQuadLeaf<U>>>> {
         let mut children: Vec<Box<dyn GeoSpacialQuadLeaf<U>>> = Vec::with_capacity(4);
         for i in 0..4 {
@@ -94,8 +103,12 @@ where
         Some(children)
     }
 
+    /// Get a custom value of a leaf.
     fn get(&self, handle: u64) -> Option<&T>;
 
+    /// Get a custom value of a leaf with mutation.
     fn get_mut(&mut self, handle: u64) -> Option<&mut T>;
+
+    /// Get a leaf by a leaf's handle.
     fn remove(&mut self, handle: u64) -> bool;
 }
