d6b17c9d1e52f6fa1ace1e04504809438990b796
keiya01 <34934510+keiya01@users.noreply.github.com>
2024-07-18 17:44:52 +0900

    fix

--- navara_ecs/src/map/event.rs
+++ navara_ecs/src/map/event.rs
@@ -1,6 +1,6 @@
 use bevy_ecs::prelude::*;
 
-pub use navara_layer::{LayerDescription, TerrainDataType, TerrainLayer, TilesLayer};
+pub use navara_layer::LayerDescription;
 
 #[derive(Debug, Clone, PartialEq, Event)]
 pub struct AddLayerEvent(pub LayerDescription);
@@ -9,42 +9,11 @@ pub fn process_add_events(mut commands: Commands, mut events: EventReader<AddLay
     for ev in events.read() {
         let AddLayerEvent(desc) = ev;
         match desc {
-            LayerDescription::Tiles {
-                url,
-                segments,
-                color,
-                max_sse,
-                max_z,
-                wireframe,
-            } => {
-                commands.spawn(TilesLayer {
-                    url: url.clone(),
-                    segments: *segments,
-                    color: *color,
-                    max_sse: *max_sse,
-                    max_z: *max_z,
-                    wireframe: *wireframe,
-                });
+            LayerDescription::Tiles(t) => {
+                commands.spawn(t.clone());
             }
-            LayerDescription::Terrain {
-                url,
-                segments,
-                color,
-                max_sse,
-                max_z,
-                wireframe,
-                elevation_decoder,
-            } => {
-                commands.spawn(TerrainLayer {
-                    url: url.clone(),
-                    segments: *segments,
-                    color: *color,
-                    max_sse: *max_sse,
-                    max_z: *max_z,
-                    wireframe: *wireframe,
-                    elevation_decoder: elevation_decoder.clone(),
-                    terrain_type: TerrainDataType::from_url(url),
-                });
+            LayerDescription::Terrain(t) => {
+                commands.spawn(t.clone());
             }
         }
     }
