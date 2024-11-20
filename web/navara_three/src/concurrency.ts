079f160f160446b0eeab5310a33b2dc2b54b59b8
keiya sasaki <34934510+keiya01@users.noreply.github.com>
2024-11-20 17:54:02 +0900

    fix: change concurrency

--- web/navara_three/src/concurrency.ts
+++ web/navara_three/src/concurrency.ts
@@ -1,3 +1,3 @@
-const TOTAL_CONCURRENCY = navigator.hardwareConcurrency - 1;
-export const FEATURE_CONCURRENCY = Math.floor(TOTAL_CONCURRENCY / 3);
-export const MAP_CONCURRENCY = TOTAL_CONCURRENCY - FEATURE_CONCURRENCY;
+const TOTAL_CONCURRENCY = Manavigator.hardwareConcurrency - 1 || 1;
+export const FEATURE_CONCURRENCY = Math.floor(TOTAL_CONCURRENCY / 3) || 1;
+export const MAP_CONCURRENCY = TOTAL_CONCURRENCY - FEATURE_CONCURRENCY || 1;
