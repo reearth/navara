43b7b7c429d5abaf93b0ab34b47c8116662cd035
keiya01 <34934510+keiya01@users.noreply.github.com>
2024-12-23 12:39:46 +0900

    fix: add vertices and geometries monitor

--- web/navara_three/src/stats.ts
+++ web/navara_three/src/stats.ts
@@ -1,30 +1,51 @@
 import Stats from "stats.js";
 
 type Renderer = {
-  beginDrawCalls: () => void;
-  endDrawCalls: () => number;
+  beginRender: () => void;
+  endRender: () => {
+    calls?: number;
+    triangles?: number;
+    memGeometries?: number;
+  };
 };
 
 export class RendererStats {
   stats: Stats;
   renderer: Renderer;
   drawCalls: Stats.Panel;
+  numTriangles: Stats.Panel;
+  memGeometries: Stats.Panel;
   constructor(r: Renderer) {
     this.stats = new Stats();
     this.renderer = r;
     this.drawCalls = this.stats.addPanel(
       new Stats.Panel("Draws", "#0ff", "#002"),
     );
+    this.numTriangles = this.stats.addPanel(
+      new Stats.Panel("Triangles", "#0fff57", "#013a12"),
+    );
+    this.memGeometries = this.stats.addPanel(
+      new Stats.Panel("Geometries", "#ffa100", "#301e00"),
+    );
     this.stats.showPanel(0);
   }
 
   begin() {
-    this.renderer.beginDrawCalls();
+    this.renderer.beginRender();
     this.stats.begin();
   }
 
   end() {
-    this.drawCalls.update(this.renderer.endDrawCalls(), 500);
+    const info = this.renderer.endRender();
+    if (info.calls) {
+      this.drawCalls.update(info.calls, 500);
+    }
+    if (info.triangles) {
+      this.numTriangles.update(info.triangles, 500);
+    }
+    if (info.memGeometries) {
+      this.memGeometries.update(info.memGeometries, 500);
+    }
     this.stats.end();
   }
 
