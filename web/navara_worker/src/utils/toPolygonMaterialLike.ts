3dc5b19b3179d03f3ac1a529d62c69130a8e579e
keiya01 <34934510+keiya01@users.noreply.github.com>
2025-08-04 16:43:31 +0900

    fix build

--- web/navara_worker/src/utils/toPolygonMaterialLike.ts
+++ web/navara_worker/src/utils/toPolygonMaterialLike.ts
@@ -4,6 +4,8 @@ import { PolygonMaterial } from "@navara/engine-worker";
 export function toPolygonMaterialLike(like: PolygonMaterialLike) {
   return new PolygonMaterial(
     like.show,
+    like.cast_shadow,
+    like.receive_shadow,
     like.color,
     like.clamp_to_ground,
     like.use_ground_normals,
