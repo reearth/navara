2e1756a5fb8bd07cd37e8d02805a9d693e69e2d6
keiya01 <34934510+keiya01@users.noreply.github.com>
2025-10-02 12:11:05 +0900

    fix: ts build error

--- web/vite.config.common.ts
+++ web/vite.config.common.ts
@@ -30,7 +30,7 @@ export const commonConfig = (name: string, env: ConfigEnv): UserConfig => ({
   plugins: [
     watchPackages(["navara_wasm", "navara_wasm_worker", "navara_wasm_api"]),
     tsconfig(),
-    dts({ rollupTypes: true }),
+    dts(),
   ],
   resolve: {
     mainFields: ["module"],
