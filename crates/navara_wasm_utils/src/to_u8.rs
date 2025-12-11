c1e546d3c23761862e9dcbf9e119516fce5c0422
adel-elmala <adel.elmala2025@gmail.com>
2025-12-11 14:23:42 +0200

    fix

--- crates/navara_wasm_utils/src/to_u8.rs
+++ crates/navara_wasm_utils/src/to_u8.rs
@@ -8,6 +8,6 @@ impl ToU8 for bool {
     // This is alway valid.
     // Ref: https://doc.rust-lang.org/reference/types/boolean.html#bit-validity
     fn to_u8(&self) -> u8 {
-        unsafe { transmute::<bool, u8>(*self) }
+        unsafe { u8::from(*self) }
     }
 }
