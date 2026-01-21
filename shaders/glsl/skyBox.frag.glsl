1dcac18be1bd05a5243b6fc2539d9c1bdc8fecba
adel-elmala <adel.elmala2025@gmail.com>
2026-01-21 14:42:38 +0200

    wip

--- shaders/glsl/skyBox.frag.glsl
+++ shaders/glsl/skyBox.frag.glsl
@@ -19,10 +19,10 @@ float dither(vec2 uv) {
 void main() {
 
     float cameraAltitude = v_cameraPositionLLA.z;
-    if (cameraAltitude >= ATMOSPHERE_CUTOFF_ALTITUDE_HIGH) {
-        gl_FragColor = vec4(0.0);
-        return;
-    }
+    // if (cameraAltitude >= ATMOSPHERE_CUTOFF_ALTITUDE_HIGH) {
+    //     gl_FragColor = vec4(0.0);
+    //     return;
+    // }
 
     float cameraAltitudeFactor = clamp((cameraAltitude - ATMOSPHERE_CUTOFF_ALTITUDE_LOW) / (ATMOSPHERE_CUTOFF_ALTITUDE_HIGH - ATMOSPHERE_CUTOFF_ALTITUDE_LOW), 0.0, 1.0);
 
@@ -35,14 +35,17 @@ void main() {
 
     float sunBlendFactor = dot(cameraDir, sunDir) * 0.5 + 0.5;
 
+    // move this to vertex shader?
     vec4 sunPosWorld = inverse(viewMatrix) * vec4(uSunDirView, 0.0);
-    float factor = dot(normalize(cameraPosition), normalize(sunPosWorld.xyz )) * 0.5 + 0.5;
+    float dayNightFactor = dot(normalize(cameraPosition), normalize(sunPosWorld.xyz )) * 0.5 + 0.5;
 
-    vec3 sunColor = mix(vec3(0.0), vec3(1.0), pow(abs(sunInSightFactor), 14.0));
-    vec3 dayColorFinal = sunColor + uDayColor;
+    vec3 sunColor = mix(vec3(0.0), vec3(1.0), pow(sunInSightFactor, 14.0));
+    vec3 dayColorFinal = sunColor + (uDayColor * 1.0 ) ;
 
-    vec4 color = vec4(mix(uNightColor, dayColorFinal, factor), 0.5);
-    vec4 result = mix(color, vec4(0.0), cameraAltitudeFactor);
+    vec4 color = vec4(mix(uNightColor, dayColorFinal, smoothstep(0.4, 0.6, dayNightFactor)), 0.4);
+    // vec4 color = vec4(mix(uNightColor, dayColorFinal, factor), 0.5);
+
+    vec4 result = mix(color, vec4(0.0), 0.0);
     result.rgb += ((dither(v_uv) - 0.5) * (1.0 / 255.0));
     // gl_FragColor = vec4(dayColorFinal, 1.0);
     gl_FragColor = result;
