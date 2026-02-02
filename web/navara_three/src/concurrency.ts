f86810a5b3a765710339a6983c4510c89b437ff3
keiya01 <keiya.s.0210@gmail.com>
2026-02-02 18:06:51 +0900

    remove unnecessary code comment

--- web/navara_three/src/concurrency.ts
+++ web/navara_three/src/concurrency.ts
@@ -10,7 +10,6 @@ const TOTAL_CONCURRENCY = isMobileDevice()
   ? Math.min(hardwareConcurrency, MOBILE_MAX_WORKERS)
   : hardwareConcurrency;
 
-// const FEATURE_CONCURRENCY = Math.max(Math.floor(TOTAL_CONCURRENCY), 1);
 const MAP_CONCURRENCY = Math.max(TOTAL_CONCURRENCY, 1);
 
 export const createDefaultConcurrencyManager = () =>
