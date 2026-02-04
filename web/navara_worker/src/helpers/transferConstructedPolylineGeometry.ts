7b6eeace570d95623ae04521ce7ed444cecbda36
Hao <38196682+stoneshell@users.noreply.github.com>
2026-02-04 08:43:33 +0800

    feat: Apply RTE to Polyline (#413)

--- web/navara_worker/src/helpers/transferConstructedPolylineGeometry.ts
+++ web/navara_worker/src/helpers/transferConstructedPolylineGeometry.ts
@@ -9,7 +9,13 @@ export function transferConstructedPolylineGeometry(
     result: like,
     transfers: [
       like.position.buffer,
+      like.position_high?.buffer,
+      like.position_low?.buffer,
       like.start.buffer,
+      like.start_high?.buffer,
+      like.start_low?.buffer,
+      like.end_high?.buffer,
+      like.end_low?.buffer,
       like.start_normals.buffer,
       like.forward_offset.buffer,
       like.end_normal_and_texture_coordinate_normalization_x.buffer,
