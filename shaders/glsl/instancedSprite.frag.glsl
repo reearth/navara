33682ff0a43cac2a1942cadf83ef5fced488609c
adel-elmala <adel.elmala2025@gmail.com>
2026-02-13 11:40:17 +0200

    fx: add a tint color to the billboard

--- shaders/glsl/instancedSprite.frag.glsl
+++ shaders/glsl/instancedSprite.frag.glsl
@@ -44,6 +44,8 @@ void main() {
     #ifdef BILLBOARD
         // Sample the specific layer from the Texture Array
         vec4 color = texture(uTexture, vec3(vUv, vLayer));
+        vec4 tint = vec4(vColor, color.a);
+        color = mix(color, tint, 0.1);
         alpha = color.a;
     #else
         alpha = nvr_circle_alpha(vUv - vec2(0.5));
