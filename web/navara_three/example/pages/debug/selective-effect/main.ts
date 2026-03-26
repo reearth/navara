6e3e3249ed2607c6644a2d35794659c24ed5dc69
Otokami-Orokabu <Otokami-Orokabu@users.noreply.github.com>
2026-03-26 09:16:59 +0900

    chore: remove old selective effect example pages
    Replaced by selective-effect2 debug example.

--- web/navara_three/example/pages/debug/selective-effect/main.ts
+++
@@ -1,9 +0,0 @@
-import ThreeView from "@navara/three";
-
-import { run, type LayerDescriptions } from "./run";
-
-const view = new ThreeView<LayerDescriptions>({
-  debug: true,
-  shadow: true,
-});
-run(view);
