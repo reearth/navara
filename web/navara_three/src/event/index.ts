04500ccfc6770edf116e2d6c0173aa469060c893
keiya01 <keiya.s.0210@gmail.com>
2026-04-09 17:40:08 +0900

    fix: improve 3dtiles traversal and maximum request

--- web/navara_three/src/event/index.ts
+++ web/navara_three/src/event/index.ts
@@ -270,10 +270,11 @@ export function processEvent(ctx: EventContext, event: Events | undefined) {
       }
     },
     {
-      shouldProcess: ({ type }) => {
+      shouldProcess: ({ type, event }) => {
         switch (type) {
           case "add":
-            return canWorkerProcessImmediately();
+            // The image extension needs worker.
+            return !IMAGE_EXTENSIONS.includes(event.extension) || canWorkerProcessImmediately();
           default:
             return true;
         }
@@ -456,7 +457,6 @@ async function processRequestedData(ctx: EventContext, req: DataRequestEvent) {
     return;
   }
 
-  // TODO: Handle abort
   await fetch(req.url, { signal: abortController.signal })
     .then((res) => {
       if (!res.ok) throw new Error();
