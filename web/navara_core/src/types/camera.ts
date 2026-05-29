faf6bbce1f4b94da8adce7c048d45a08df7af4c3
keiya01 <keiya.s.0210@gmail.com>
2026-05-29 12:41:24 +0900

    fix reviews

--- web/navara_core/src/types/camera.ts
+++ web/navara_core/src/types/camera.ts
@@ -8,6 +8,7 @@ export type CameraOrientation = Partial<
 >;
 
 export type CameraPosition = Partial<LatLngHeight> &
+  CameraOrientation & {
     /** Distance from the target point (lng/lat/height) along the camera forward direction (meters).
      *  When specified, the camera is placed so its forward ray reaches the target point from this distance.
      *  If `height` is omitted, the target elevation defaults to 0 (sea level). */
